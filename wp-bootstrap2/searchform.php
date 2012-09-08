f6bb05765b75b07aacc07585a250cd8c6696efe3
Vino Rodrigues <github@m.vinorodrigues.com>
2012-09-08 22:01:04 +1000

    simplified search form

--- wp-bootstrap2/searchform.php
+++ wp-bootstrap2/searchform.php
@@ -7,16 +7,8 @@
  * @since WP-Bootstrap2 0.1
  */
 ?>
-<form method="get" id="searchform" action="<?php echo esc_url( home_url( '/' ) ); ?>" role="search" class="form-inline">
-	<fieldset>
-		<div class="control-group">
-			<label for="s" class="control-label assistive-text"><?php _e( 'Search', 'bootstrap2' ); ?></label>
-			<div class="controls">
-				<div class="input-append">
-					<input type="search" class="field search-query" name="s" id="s" placeholder="<?php esc_attr_e( 'Search &hellip;', 'bootstrap2' ); ?>" results="5" /><button
-						type="submit" class="submit btn" name="submit" id="searchsubmit" value="<?php esc_attr_e( 'Search', 'bootstrap2' ); ?>" /><i class="icon-search"></i></button>
-				</div>
-			</div>
-		</div>
-	</fieldset>
+<form method="get" id="searchform" action="<?php echo esc_url( home_url( '/' ) ); ?>" role="search" class="form-search">
+	<label for="s" class="control-label assistive-text"><?php _e( 'Search', 'bootstrap2' ); ?></label>
+	<input type="search" class="field input-medium search-query" name="s" id="s" placeholder="<?php esc_attr_e( 'Search &hellip;', 'bootstrap2' ); ?>" results="5" />
+	<button type="submit" class="submit btn" name="submit" id="searchsubmit" value="<?php esc_attr_e( 'Search', 'bootstrap2' ); ?>" /><i class="icon-search"></i></button>
 </form>
